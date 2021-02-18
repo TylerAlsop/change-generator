f513d3d0162a5d8079d8bb3b37c52fb4816d6f9e
Tyler Alsop <tylerjalsop@gmail.com>
2021-02-18 09:48:46 -0700

    Added test to for function to calculate number of quarters. Failing as expected.

--- __test__/change.test.js
+++ __test__/change.test.js
@@ -3,6 +3,15 @@ const returnChange = require("../changeReturn")
 
 describe("Getting the correct change in return.", () => {
 
+    //////////// 44 Cents //////////////
+    it("Should return 1 quarter, 1 dimes, 1 nickel, and 4 pennies.", () => {
+        const result = returnChange(44)
+        expect(result.quarters).toBe(1)
+        expect(result.dimes).toBe(1)
+        expect(result.nickels).toBe(1)
+        expect(result.pennies).toBe(4)
+        })
+
     //////////// 28 Cents //////////////
     it("Should return 2 dimes, 1 nickel, and 3 pennies.", () => {
         const result = returnChange(28)
