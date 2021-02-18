180699cd98d58b9a2f3de84ac01f93a63cac5728
Tyler Alsop <tylerjalsop@gmail.com>
2021-02-18 10:38:28 -0700

    Reorganized and relabeled tests for better readability.

--- __test__/change.test.js
+++ __test__/change.test.js
@@ -3,7 +3,7 @@ const returnChange = require("../changeReturn")
 
 describe("Getting the correct change in return.", () => {
 
-    //////////// 193 Cents //////////////
+    //////////// QUARTERS & BELOW //////////////
     it("Should return 7 quarters, 1 dimes, 1 nickel, and 3 pennies.", () => {
         const result = returnChange(193)
         expect(result.quarters).toBe(7)
@@ -13,7 +13,6 @@ describe("Getting the correct change in return.", () => {
         })
     
 
-    //////////// 44 Cents //////////////
     it("Should return 1 quarter, 1 dimes, 1 nickel, and 4 pennies.", () => {
         const result = returnChange(44)
         expect(result.quarters).toBe(1)
@@ -22,8 +21,7 @@ describe("Getting the correct change in return.", () => {
         expect(result.pennies).toBe(4)
         })
 
-    //////////// 28 Cents //////////////
-    it("Should return 2 dimes, 1 nickel, and 3 pennies.", () => {
+    it("Should return 1 quarter, 0 dimes, 0 nickels, and 3 pennies.", () => {
         const result = returnChange(28)
         expect(result.quarters).toBe(1)
         expect(result.dimes).toBe(0)
@@ -31,17 +29,17 @@ describe("Getting the correct change in return.", () => {
         expect(result.pennies).toBe(3)
     })
 
-    //////////// 2 Cents //////////////
-    it("Should return 2 dimes, 1 nickel, and 3 pennies.", () => {
-        const result = returnChange(28)
-        expect(result.quarters).toBe(1)
-        expect(result.dimes).toBe(0)
-        expect(result.nickels).toBe(0)
-        expect(result.pennies).toBe(3)
+    //////////// DIMES & BELOW //////////////
+    it("Should return 1 dime, 1 nickel, and 2 pennies.", () => {
+        const result = returnChange(17)
+        expect(result.quarters).toBe(0)
+        expect(result.dimes).toBe(1)
+        expect(result.nickels).toBe(1)
+        expect(result.pennies).toBe(2)
     })
 
 
-    ////////////// 7 cents //////////////
+    ////////////// NICKELS & BELOW //////////////
     it("Should return 1 nickel and 2 pennies", () => {
         const result = returnChange(7)
         expect(result.quarters).toBe(0)
@@ -51,7 +49,7 @@ describe("Getting the correct change in return.", () => {
     })
 
     
-    //////////// 4 cents //////////////
+    //////////// PENNIES //////////////
 
     it("Should return 4 pennies.", () => {
         const result = returnChange(4)
