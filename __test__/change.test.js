b61e01bee4dc9c4e3c8ce163708a38e1b200acc2
Tyler Alsop <tylerjalsop@gmail.com>
2021-02-18 09:24:12 -0700

    Uncommented the test for dimes. Failing as expected.

--- __test__/change.test.js
+++ __test__/change.test.js
@@ -3,12 +3,20 @@ const returnChange = require("../changeReturn")
 
 describe("Getting the correct change in return.", () => {
 
-    ////////////// 22 Cents //////////////
-    // it("Should return 2 dimes and 2 pennies.", () => {
-    //     const result = returnChange(22)
-    //     expect(result.dimes).toBe(2)
-    //     expect(result.nickels).toBe(0)
-    //     expect(result.pennies).toBe(2)
+    //////////// 22 Cents //////////////
+    it("Should return 2 dimes and 2 pennies.", () => {
+        const result = returnChange(22)
+        expect(result.dimes).toBe(2)
+        expect(result.nickels).toBe(0)
+        expect(result.pennies).toBe(2)
+    })
+
+
+    // ////////////// 14 cents //////////////
+    // it("Should return 2 nickels and 4 pennies", () => {
+    //     const result = returnChange(14)
+    //     expect(result.nickels).toBe(2)
+    //     expect(result.pennies).toBe(4)
     // })
 
 
@@ -17,13 +25,6 @@ describe("Getting the correct change in return.", () => {
         const result = returnChange(7)
         expect(result.nickels).toBe(1)
         expect(result.pennies).toBe(2)
-    })
-
-    ////////////// 14 cents //////////////
-    it("Should return 2 nickels and 4 pennies", () => {
-        const result = returnChange(14)
-        expect(result.nickels).toBe(2)
-        expect(result.pennies).toBe(4)
     })
 
     
